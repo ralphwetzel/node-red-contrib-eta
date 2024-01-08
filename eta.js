78a8cb690a63fba181e33c94aa5a32f3e20a8468
ralphwetzel <theonionbox@gmx.com>
2024-01-08 21:07:11 +0100

    v2.0.0

--- eta.js
+++ eta.js
@@ -1,67 +1,65 @@
-Eta = require("eta")
+const _Eta = require("eta")
+class Eta extends _Eta.Eta {
+    // This ensures, that no file system acces is done.
+    // Additionally, it eliminates the demand to pre-pend non-file templates with "@"
+    resolvePath = null;
+  }
 
 module.exports = function(RED) {
     function etaTemplate(config) {
 
-        function get_template(templates, id) {
-            for (let tpl of templates) {
-                if (tpl.id === id) {
-                    return tpl;
-                }
-            }
-            return undefined;
-        }
-        
         RED.nodes.createNode(this,config);
-        var node = this;
+        let node = this;
 
-        // create this node's own template cache
-        node.template_cache = new Eta.config.templates.constructor({});
-
-        node.eta_config = Eta.getConfig({
-            templates: node.template_cache,
+        const eta = new Eta({
             autoTrim: false,
-            includeFile: undefined,
-            include: function custom_include(templateName, data) {
-
-                // this overrides the Eta standard inplementation
-                // ... to compile user provided templates on demand!
-            
-                // check if template cache knows templateName
-                const template = this.templates.get(templateName)
-                if (!template) {
-                    // if not: check if there's a template source with id: templateName
-                    let tpl = get_template(config.templates, templateName)
-                    if (tpl) {
-                        // compile this template source
-                        this.templates.define(templateName, Eta.compile(tpl.template, this));
-                    }
-                    
-                    // Use the original Eta.include function to
-                    // retry to get the template from the cache
-                    // ... and throw a nice error if still not found.
-
-                    return Eta.config.include.call(this, templateName, data);
-                }
-                return template(data, this);
-            }    
+            varName: "msg",      // no more 'it.msg' !!
+            functionHeader: "const it = { 'msg': msg };"  // offer 'it.msg' as an alternative to the new 'msg' for backward compatibility !!
         })
+
+        // only considered for __root__
+        let _async = false;
+
+        // preload the templates
+        if (config.templates && Array.isArray(config.templates)) {
+            config.templates.forEach( (tpl) => {
+                if (tpl.id == "__root__") {
+                    _async = tpl.async ?? false;
+                }
+                eta.loadTemplate(tpl.id, tpl.template, {async: tpl.async} );
+            });
+        }
 
         node.on('input', function(msg, send, done) {
 
             send = send || function() { node.send.apply(node,arguments) }
 
-            let tpl = get_template(config.templates, "__root__")
-            if (tpl)
-            {
-                // msg will be accessible in the template via it.msg!
-                let result = Eta.render(tpl.template, {msg: msg}, node.eta_config);
-                msg.payload = result;
+            if (_async) {
+                if (!done) {
+                    node.warn("'done()' is undefined. Cannot perform async template rendering.");
+                    return;
+                }
+    
+                eta.renderAsync("__root__", msg)
+                .then((result) => {
+                    msg.payload = result;
+                    send(msg);
+                    done();
+                })
+                .catch((err) => {
+                    done(err);
+                })
+                return;
+            }
+
+            try {
+                msg.payload = eta.render("__root__", msg);
                 send(msg);
+            } catch (err) {
+                node.error(err.message);
             }
-            if (done) {
-                done();
-            }
+            if (done) done();
+            return;
         });
     }
     RED.nodes.registerType("etaTemplate",etaTemplate);
